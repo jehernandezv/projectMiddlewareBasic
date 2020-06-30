173b77bf0bfe1dfcb9a5b433466c371200d82d42
unknown <cristian.lopez04@uptc.edu.co>
2020-06-29 23:52:21 -0500

    Se agrego la conexión con servidores

--- WEB-INF/classes/DemoServ.java
+++ WEB-INF/classes/DemoServ.java
@@ -1,11 +1,21 @@
-import javax.servlet.http.*;
-import javax.servlet.*;
-import java.io.*;
-public class DemoServ extends HttpServlet{
-public void doGet(HttpServletRequest req,HttpServletResponse res)throws ServletException,IOException
-{
-res.setContentType("text/html");
-PrintWriter pw=res.getWriter();
-String name=req.getParameter("name");
-pw.println("Welcome "+name);
-}}
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DemoServ extends HttpServlet {
+
+	private static final long serialVersionUID = -4440011616187444440L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		res.setContentType("text/html");
+		PrintWriter pw = res.getWriter();
+		String name = req.getParameter("name");
+		pw.println("Welcome " + name);
+		pw.println("Nice");
+	}
+
+}
